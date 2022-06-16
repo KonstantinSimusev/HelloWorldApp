2ddd40524588b665b5bc53a34445cbd782a280a7
Konstantin <simussev.k@icloud.com>
2022-06-16 11:31:36 +0500

    Work with logic

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setLabelSettings()
+        setButtonSettings()
     }
 
-
+    @IBAction func startPressed() {
+        helloWorldLabel.isHidden.toggle()
+        
+        if helloWorldLabel.isHidden {
+            startButton.setTitle("Show text", for: .normal)
+        } else {
+            startButton.setTitle("Hide text", for: .normal)
+        }
+    }
+    
+    private func setLabelSettings() {
+        helloWorldLabel.isHidden = true
+        helloWorldLabel.textColor = .systemPink
+    }
+    
+    private func setButtonSettings() {
+        startButton.setTitle("Start the game", for: .normal)
+        startButton.layer.cornerRadius = 13
+        
+    }
+    
 }
 
